13211deb28286c2c97c59a99cda3fbb9e46e32a1
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-05-27 21:07:57 +0400

    changed priority que

--- queue.js
+++ queue.js
@@ -1,24 +1,33 @@
 function PriorityQueue() {
     this.collection = [];
     this.length = 0;
+
     this.dequeue = function() {
-        this.length--;
-        return this.collection.shift();
+       //removes first element from collection
+       if(!this.length){
+          return undefined
+       } else {
+          this.length--;
+          return this.collection.shift();
+       }
     };
-    this.enqueue = function (data){
-        if(this.length === 0){
-            this.collection.push(data);
+
+    this.enqueue = function (newItem){
+        if(!this.length){
+           this.collection.push(newItem);
         } else {
             let added = false;
+            //looping threw the collection, to put priotiezed item
             for(let i = 0; i < this.collection.length; i++) {
-                if(data[1] < this.collection[i][1]){
-                    this.collection.splice(i, 0, data);
+               if(newItem[1] < this.collection[i][1]){
+                  this.collection.splice(i, 0, newItem);
                     added = true;
                     break;
                 }
             }
+           //if the priority item is not included in collection just push it to the end
             if(!added){
-                this.collection.push(data);
+               this.collection.push(newItem);
             }
         }
         this.length++;
