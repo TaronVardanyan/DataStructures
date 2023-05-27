4b2a296cea33ea3dc2d039053717280bc3460009
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-05-27 18:11:16 +0400

    updated stack

--- stack.js
+++ stack.js
@@ -1,21 +1,29 @@
 function Stack() {
-    this.count = 0;
-    this.storage = {};
-    this.push = function (data) {
-        this.storage[this.count] = data;
-        this.count++;
-    };
-    this.pop = function () {
-        if(this.count === 0) {
-            return undefined;
-        } else {
-            let lastItem = this.storage[this.count - 1];
-            delete this.storage[this.count - 1];
-            this.count--;
-            return lastItem;
-        }
-    };
-    this.peek = function () {
-        return this.storage[this.count - 1];
-    };
+   this.count = 0;
+   this.storage = {};
+
+   this.push = function (newItem) {
+      //adding new item to stack
+      this.count++;
+      this.storage[this.count - 1] = newItem;
+      return this.storage;
+   };
+
+   this.pop = function () {
+      //remove the last item from stack;
+      if(!this.count){
+         //check if stack is empty
+         return undefined;
+      } else {
+         //saving the referance of last item in const
+         const lastItem = this.storage[this.count - 1];
+         delete this.storage[this.count - 1];
+         this.count--;
+         return lastItem;
+      }
+   };
+   this.getLastItem = function () {
+      //return the last item
+      return this.storage[this.count - 1];
+   }
 }
